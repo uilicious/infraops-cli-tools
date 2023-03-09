89db66729a720f4a28a9ee7741669de9b207d95f
Eugene Cheah <eugeneqin@gmail.com>
2023-03-09 08:51:33 +0000

    fix cloudflared for macos (need to test)

--- cloudflared.sh
+++ cloudflared.sh
@@ -40,21 +40,22 @@ UNAME_A="$(uname -a)"
 # Lets detect linux or mac os platform
 OS_PLATFORM="linux"
 
-# if [ "$UNAME_A" == *"Darwin"* ]; then
-#     # # Use the linus build, for the macos environment
-# 	# OS_PLATFORM="darwin"
-# elif [ "$UNAME_A" == *"Linux"* ]; then
-#     # Do something under GNU/Linux platform
-# elif [ "$UNAME_A" == *"MINGW32_NT"* ]; then
-#     # Do something under 32 bits Windows NT platform
-# elif [ "$UNAME_A" == *"MINGW64_NT"* ]; then
-#     # Do something under 64 bits Windows NT platform
-# fi
-
 # Lets detet the architecture
 OS_ARCH="amd64"
 if [ "$UNAME_A" == *"arm64"* ]; then
 	OS_ARCH="arm64"
+fi
+
+if [ "$UNAME_A" == *"Darwin"* ]; then
+    # Use the darwin x64 build, for the macos environment
+	OS_PLATFORM="darwin"
+    OS_ARCH="amd64"
+elif [ "$UNAME_A" == *"Linux"* ]; then
+    # Do something under GNU/Linux platform
+elif [ "$UNAME_A" == *"MINGW32_NT"* ]; then
+    # Do something under 32 bits Windows NT platform
+elif [ "$UNAME_A" == *"MINGW64_NT"* ]; then
+    # Do something under 64 bits Windows NT platform
 fi
 
 # Ensure the dir exists
