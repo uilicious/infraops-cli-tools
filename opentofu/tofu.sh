cb93ac69d7303f5d136b079e1e053ab56058a29e
@picocreator (Eugene Cheah) <eugeneqin@gmail.com>
2023-11-22 14:14:13 -0800

    Adding opentofu support (fork of teraform)

---
+++ opentofu/tofu.sh
@@ -0,0 +1,65 @@
+#!/bin/sh
+set -e
+
+# Version / OS / Arch
+TOFU_VERSION="1.6.0-alpha5"
+OS="$(uname | tr '[:upper:]' '[:lower:]')"
+ARCH="$(uname -m | sed -e 's/aarch64/arm64/' -e 's/x86_64/amd64/')"
+
+# The current working directory
+CUR_WRK_DIR="$(pwd)"
+
+# Get the current script dir
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+# The directory path
+CLI_BIN_DIR="$SCRIPT_DIR/.bin/$OS/$TOFU_VERSION"
+# The final CLI bin path
+CLI_BIN_PATH="$CLI_BIN_DIR/tofu"
+
+# The download URL
+CLI_DOWNLOAD_URL="https://github.com/opentofu/opentofu/releases/download/v${TOFU_VERSION}/tofu_${TOFU_VERSION}_${OS}_${ARCH}.zip"
+
+# Download the file if needed
+if [ ! -f "$CLI_BIN_PATH" ]; then
+
+	# Ensure the dir path exists
+	if [ ! -d "$CLI_BIN_DIR" ]; then
+		mkdir -p "$CLI_BIN_DIR"
+	fi
+	cd "$CLI_BIN_DIR" 
+	
+	# Download an unzip / untar
+	curl -LO "$CLI_DOWNLOAD_URL"
+	unzip "tofu_${TOFU_VERSION}_${OS}_${ARCH}.zip"
+
+	# Remove the old zip file
+	rm "tofu_${TOFU_VERSION}_${OS}_${ARCH}.zip"
+
+	# Reset to working dir
+	cd "$CUR_WRK_DIR"
+fi
+chmod +x "$CLI_BIN_PATH"
+
+# Execute the file respectively
+"$CLI_BIN_PATH" "$@"
+
+
+
+
+
+
+
+
+
+
+
+
+# TEMPDIR="$(mktemp -d)"
+# pushd "${TEMPDIR}" >/dev/null
+# wget "https://github.com/opentofu/opentofu/releases/download/v${TOFU_VERSION}/tofu_${TOFU_VERSION}_${OS}_${ARCH}.zip"
+# unzip "tofu_${TOFU_VERSION}_${OS}_${ARCH}.zip"
+# sudo mv tofu /usr/local/bin/tofu
+# popd >/dev/null
+# rm -rf "${TEMPDIR}"
+# echo "OpenTofu is now available at /usr/local/bin/tofu."
